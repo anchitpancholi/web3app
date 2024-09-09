e1b95e7e582e191f74387fcf7fa658dfc3a95c7a
Anchit Pancholi <134777300+anchitpancholi@users.noreply.github.com>
2024-09-09 18:31:01 +0530

    update

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./AccountTransactions.json";
 
-export const contractAddress = "0x0358E86729051c8904ee413B5e68B0fE0A07B56A";
+export const contractAddress = "<<YOUR_CONTRACT_ADDRESS>>";
 export const contractABI = abi.abi;
